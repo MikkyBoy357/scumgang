e1663315618a788ecb69106372fa5d4fcad359a9
Michael Olusegun <mikkyboy2005@gmail.com>
2021-02-09 12:23:38 +0100

    category_view listview fixed

---
+++ lib/screens/home/components/cart_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:saydo/design_system/button_widgets/buttons/blue_buttons/button1.dart';
+import 'package:saydo/design_system/colors/colors.dart';
+import 'package:saydo/design_system/widgets/cart_item_cards/cart_item.dart';
+
+class Cart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Cart',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(vertical: 20.0),
+            //   child: Text(
+            //     'Category',
+            //     style: TextStyle(
+            //       color: MyColors.black2,
+            //       fontSize: 36,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 6,
+                // ignore: missing_return
+                itemBuilder: (context, index) {
+                  // return CartItem();
+                  if (index != 0) {
+                    return CartItem();
+                  } else {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Text(
+                        'Category',
+                        style: TextStyle(
+                          color: MyColors.black2,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          border: Border.symmetric(
+            horizontal: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Total Price',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Button1(
+                label: 'Confirm Order',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
