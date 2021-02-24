05e482b10c4f262a0d12a43a35d0bed2ea26b7e1
Michael Olusegun <mikkyboy2005@gmail.com>
2021-02-24 19:29:56 +0100

    categories and categories_view firestore done

---
+++ lib/screens/home/components/pageviews/category_view.dart
@@ -0,0 +1,183 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:saydo/design_system/colors/colors.dart';
+import 'package:saydo/screens/home/components/add_to_cart.dart';
+
+class CategoryView extends StatelessWidget {
+  final String categoryName;
+
+  const CategoryView({Key key, this.categoryName}) : super(key: key);
+  @override
+  build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        bottom: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: Text(
+            categoryName,
+            style: TextStyle(
+              color: MyColors.black2,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection(categoryName.toLowerCase())
+                  .where('categoryId', isEqualTo: categoryName.toLowerCase())
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(child: CircularProgressIndicator());
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: snapshot.data.docs.length,
+                    // ignore: missing_return
+                    itemBuilder: (context, index) {
+                      // return CartItem();
+                      if (index != -1) {
+                        return GestureDetector(
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(16)),
+                            ),
+                            elevation: 4.0,
+                            child: Container(
+                              height: 96,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Color(0x90707070)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16)),
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 96,
+                                    width: 96,
+                                    decoration: BoxDecoration(
+                                      // border: Border.all(
+                                      //   color: Color(0x90707070),
+                                      // ),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(16)),
+                                      image: DecorationImage(
+                                        image: AssetImage('images/image.png'),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    // child: Image(
+                                    //   image: AssetImage('images/image.png'),
+                                    // ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Container(
+                                      // color: Colors.red,
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                              10,
+                                      width: MediaQuery.of(context).size.width /
+                                          2.3,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Column(
+                                            children: [
+                                              Text(
+                                                snapshot.data.docs[index]
+                                                    ['name'],
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          Container(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            'Size',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Price',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            return showModalBottomSheet(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(16),
+                                  topRight: Radius.circular(16),
+                                ),
+                              ),
+                              context: context,
+                              builder: (BuildContext context) {
+                                return DraggableScrollableSheet(
+                                  expand: true,
+                                  initialChildSize: 0.95,
+                                  maxChildSize: 1,
+                                  minChildSize: 0.95,
+                                  builder: (BuildContext context,
+                                      ScrollController scrollController) {
+                                    return AddToCart();
+                                  },
+                                );
+                              },
+                            );
+                          },
+                        );
+                      } else {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          child: Text(
+                            'Category',
+                            style: TextStyle(
+                              color: MyColors.black2,
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
